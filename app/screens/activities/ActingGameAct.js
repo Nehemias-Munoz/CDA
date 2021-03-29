e4e06e9b84d9d5efee09cf959e29bbeca6ef29cf
Nehemias-Munoz <marvic.developer@gmail.com>
2021-03-29 15:10:24 -0300

    Terminada apariencia ActingGame y correccion Submit

--- app/screens/activities/ActingGameAct.js
+++ app/screens/activities/ActingGameAct.js
@@ -1,71 +1,60 @@
 import React, { useState } from 'react';
-import { TextInput, ScrollView, StyleSheet, Text, View, ActivityIndicator, Modal, Pressable } from 'react-native';
-import { Icon, Card, Button } from 'react-native-elements';
+import { ScrollView, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { Card } from 'react-native-elements';
 //Componentes
 import EvidenceTypeBar from '../components/EvidenceTypeBar';
+import SubmitComponent from '../components/SubmitComponent';
 
 const ActingGameAct = () => {
-  //MODAL
-  const [modalVisible, setModalVisible] = useState(false);
-
+  //Tipo de componente submit
+  const [typeSubmit, setTypeSubmit] = useState();
+  //Datos Actividad
   const [activity] = useState({
     id: 1,
     title: 'Actuación',
     type: 'Link',
     description:
       'Crea un video de un minuto, donde tu y tus amigos representen el siguiente concepto:',
-    concept: 'Planeta',
+    concept: 'Celula',
     instruction:
-      'Grabe un video, suba a Youtube y comparta el link del video.',
+      'Grabe un video, suba a Youtube y comparta el link del video, ejemplo: https://youtu.be/ejemplo.',
   });
 
   return (
     <>
       <View style={styles.container}>
-        <Card>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <Card.Title style={styles.title}>{activity.title}</Card.Title>
-            <EvidenceTypeBar />
-            <Card.Image
-              style={styles.imageStyle}
-              source={require('../../../assets/images/activities/actinggame.jpg')}
-            />
-            <View style={styles.activityContent}>
-              <Text style={styles.infoTitle}>Descripcion:</Text>
-              <Text style={styles.infoDesc}>
-                {activity.description} {activity.concept}
-              </Text>
-              <View style={styles.containerConcept}>
-                <Card.Image
-                  containerStyle={styles.imageConceptContainer}
-                  PlaceholderContent={<ActivityIndicator />}
-                  style={styles.imageConceptStyle}
-                  source={require('../../../assets/images/activities/actinggame/cells.png')}
-                />
-              </View>
-              <Text style={styles.infoTitle}>Instruciones:</Text>
-              <Text style={styles.infoDesc}>{activity.instruction}</Text>
-            </View>
-            <View style={styles.containerSubmit}>
-              <TextInput style={styles.inputStyle} keyboardType={'url'} placeholder={'Copia tu link aqui'} />
-              <Button
-                icon={
-                  <Icon
-                    name="upload"
-                    type="font-awesome"
-                    size={20}
-                    color="#ffffff"
-                  />
-                }
-                titleStyle={styles.textButton}
-                containerStyle={styles.containerButton}
-                buttonStyle={styles.buttonStyle}
-                title={'Entregar'}
-                onPress={() => setModalVisible(!modalVisible)}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Card>
+            <ScrollView showsVerticalScrollIndicator={false}>
+              <Card.Title style={styles.title}>{activity.title}</Card.Title>
+              <EvidenceTypeBar
+                type={activity.type}
+                setTypeSubmit={setTypeSubmit}
               />
-            </View>
-          </ScrollView>
-        </Card>
+              <Card.Image
+                style={styles.imageStyle}
+                source={require('../../../assets/images/activities/actinggame.jpg')}
+              />
+              <View style={styles.activityContent}>
+                <Text style={styles.infoTitle}>Descripción:</Text>
+                <Text style={styles.infoDesc}>
+                  {activity.description} {activity.concept}
+                </Text>
+                <View style={styles.containerConcept}>
+                  <Card.Image
+                    containerStyle={styles.imageConceptContainer}
+                    PlaceholderContent={<ActivityIndicator />}
+                    style={styles.imageConceptStyle}
+                    source={require('../../../assets/images/activities/actinggame/cells.png')}
+                  />
+                </View>
+                <Text style={styles.infoTitle}>Instruciones:</Text>
+                <Text style={styles.infoDesc}>{activity.instruction}</Text>
+              </View>
+              {typeSubmit ? <SubmitComponent typeSubmit={typeSubmit} /> : null}
+            </ScrollView>
+          </Card>
+        </ScrollView>
       </View>
     </>
   );
@@ -76,8 +65,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   title: {
-    textTransform: 'uppercase',
     fontSize: 15,
+    textTransform: 'uppercase',
   },
   activityContent: {
     padding: 10,
@@ -89,6 +78,9 @@ const styles = StyleSheet.create({
     elevation: 2,
     shadowOffset: { width: 0, height: 2 },
   },
+  imageStyle: {
+    height: 100,
+  },
   infoTitle: {
     marginTop: 5,
     fontWeight: 'bold',
@@ -98,28 +90,6 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'normal',
     textAlign: 'justify',
-  },
-  containerSubmit: {
-    flexDirection: 'row',
-  },
-  inputStyle: {
-    height: 50,
-    flex: 2,
-    borderWidth: 0.8,
-    borderColor: '#000',
-  },
-  containerButton: {
-    flex: 1,
-  },
-  buttonStyle: {
-    height: 50,
-    backgroundColor: '#062b3c',
-  },
-  textButton: {
-    marginLeft: 3,
-  },
-  imageStyle: {
-    height: 100,
   },
   containerConcept: {
     padding: 5,
@@ -133,8 +103,6 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
-  //Estilos Modal
-
 });
 
 export default ActingGameAct;
