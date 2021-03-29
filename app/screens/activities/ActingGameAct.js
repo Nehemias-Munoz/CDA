e46f80d5d265055556b2c243d1c16250c2d95a0e
Nehemias-Munoz <marvic.developer@gmail.com>
2021-03-29 14:36:02 -0300

    Terminda Primera apariencia ActingGame

---
+++ app/screens/activities/ActingGameAct.js
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import { TextInput, ScrollView, StyleSheet, Text, View, ActivityIndicator, Modal, Pressable } from 'react-native';
+import { Icon, Card, Button } from 'react-native-elements';
+//Componentes
+import EvidenceTypeBar from '../components/EvidenceTypeBar';
+
+const ActingGameAct = () => {
+  //MODAL
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [activity] = useState({
+    id: 1,
+    title: 'Actuación',
+    type: 'Link',
+    description:
+      'Crea un video de un minuto, donde tu y tus amigos representen el siguiente concepto:',
+    concept: 'Planeta',
+    instruction:
+      'Grabe un video, suba a Youtube y comparta el link del video.',
+  });
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Card>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Card.Title style={styles.title}>{activity.title}</Card.Title>
+            <EvidenceTypeBar />
+            <Card.Image
+              style={styles.imageStyle}
+              source={require('../../../assets/images/activities/actinggame.jpg')}
+            />
+            <View style={styles.activityContent}>
+              <Text style={styles.infoTitle}>Descripcion:</Text>
+              <Text style={styles.infoDesc}>
+                {activity.description} {activity.concept}
+              </Text>
+              <View style={styles.containerConcept}>
+                <Card.Image
+                  containerStyle={styles.imageConceptContainer}
+                  PlaceholderContent={<ActivityIndicator />}
+                  style={styles.imageConceptStyle}
+                  source={require('../../../assets/images/activities/actinggame/cells.png')}
+                />
+              </View>
+              <Text style={styles.infoTitle}>Instruciones:</Text>
+              <Text style={styles.infoDesc}>{activity.instruction}</Text>
+            </View>
+            <View style={styles.containerSubmit}>
+              <TextInput style={styles.inputStyle} keyboardType={'url'} placeholder={'Copia tu link aqui'} />
+              <Button
+                icon={
+                  <Icon
+                    name="upload"
+                    type="font-awesome"
+                    size={20}
+                    color="#ffffff"
+                  />
+                }
+                titleStyle={styles.textButton}
+                containerStyle={styles.containerButton}
+                buttonStyle={styles.buttonStyle}
+                title={'Entregar'}
+                onPress={() => setModalVisible(!modalVisible)}
+              />
+            </View>
+          </ScrollView>
+        </Card>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    textTransform: 'uppercase',
+    fontSize: 15,
+  },
+  activityContent: {
+    padding: 10,
+    borderWidth: 0,
+    marginVertical: 5,
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+    shadowOffset: { width: 0, height: 2 },
+  },
+  infoTitle: {
+    marginTop: 5,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  infoDesc: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    textAlign: 'justify',
+  },
+  containerSubmit: {
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    height: 50,
+    flex: 2,
+    borderWidth: 0.8,
+    borderColor: '#000',
+  },
+  containerButton: {
+    flex: 1,
+  },
+  buttonStyle: {
+    height: 50,
+    backgroundColor: '#062b3c',
+  },
+  textButton: {
+    marginLeft: 3,
+  },
+  imageStyle: {
+    height: 100,
+  },
+  containerConcept: {
+    padding: 5,
+    alignItems: 'center',
+  },
+  imageConceptContainer: {
+    margin: 10,
+    borderRadius: 5,
+  },
+  imageConceptStyle: {
+    width: 100,
+    height: 100,
+  },
+  //Estilos Modal
+
+});
+
+export default ActingGameAct;
