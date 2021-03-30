8f001a894c7dab3af184e3e66513f462a354cf40
Nehemias-Munoz <marvic.developer@gmail.com>
2021-03-29 21:37:32 -0300

    Terminada apariencia y agregada a NavTab

--- app/screens/views/Activities.js
+++ app/screens/views/Activities.js
@@ -3,6 +3,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 //Vista de Actividades
 import ActingGameAct from '../activities/ActingGameAct';
+import DebateAct from '../activities/DebateAct';
 //Navegacion
 const Tab = createMaterialTopTabNavigator();
 
@@ -11,6 +12,7 @@ const Activities = () => {
     <SafeAreaProvider>
       <Tab.Navigator tabBarOptions={styleTabBar.tabBar}>
         <Tab.Screen name={'ActingGameAct'} component={ActingGameAct} />
+        <Tab.Screen name={'DebateAct'} component={DebateAct} />
       </Tab.Navigator>
     </SafeAreaProvider>
   );
